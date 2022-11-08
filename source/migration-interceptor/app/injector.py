d3089f74bec8b3c9cc9bcc2c33c6c5e8ff1af2f6
Jew <jewjewthanawat@gmail.com>
2022-11-08 01:20:52 -0600

    Support creating with PinD

--- source/migration-interceptor/app/injector.py
+++ source/migration-interceptor/app/injector.py
@@ -6,8 +6,8 @@ import yaml
 
 from share.const import MIGRATABLE_ANNOTATION, BYPASS_ANNOTATION, INTERFACE_ANNOTATION, INTERFACE_DIND, \
     START_MODE_ANNOTATION, START_MODE_ACTIVE, VOLUME_LIST_ANNOTATION, CONTAINER_SPEC_ANNOTATION, LAST_APPLIED_CONFIG, \
-    ORCHESTRATOR_TYPE_MINISHIFT, ORCHESTRATOR_TYPE_KUBERNETES
-from share.env import EXEC_MONITOR, IMAGE_PULL_POLICY, env, ORCHESTRATOR_TYPE
+    ORCHESTRATOR_TYPE_MINISHIFT, ORCHESTRATOR_TYPE_KUBERNETES, INTERFACE_PIND, INTERFACE_FF, MIGRATABLE_FALSE
+from share.env import AMBASSADOR_IMAGE, IMAGE_PULL_POLICY, env, ORCHESTRATOR_TYPE
 
 
 @kopf.on.mutate('v1', 'pods', operation='CREATE', annotations={BYPASS_ANNOTATION: kopf.ABSENT})
@@ -25,13 +25,24 @@ def mutate_pod(annotations, spec, patch, **_):
 
 def inject_pod(annotations, spec):
     if annotations.get(INTERFACE_ANNOTATION) == INTERFACE_DIND:
-        template = inject_pod_dind(spec)
-    else:
+        template = inject_pod_dind(spec, '../template/pod-dind.yaml')
+    elif annotations.get(INTERFACE_ANNOTATION) == INTERFACE_PIND:
+        template = inject_pod_dind(spec, '../template/pod-pind.yaml')
+    elif annotations.get(INTERFACE_ANNOTATION) == INTERFACE_FF:
         template = inject_pod_ff(spec)
-    template['metadata']['annotations'][MIGRATABLE_ANNOTATION] = str(False)
+    else:
+        template = inject_native(spec)
+    template['metadata']['annotations'][MIGRATABLE_ANNOTATION] = MIGRATABLE_FALSE
     template['metadata']['annotations'][START_MODE_ANNOTATION] = annotations.get(START_MODE_ANNOTATION,
                                                                                  START_MODE_ACTIVE)
     template['metadata']['annotations'][BYPASS_ANNOTATION] = str(True)
+    return template
+
+
+def inject_native(spec):
+    with open(os.path.join(os.path.dirname(__file__), '../template/pod.yaml'), 'rt') as f:
+        template = yaml.safe_load(f.read().format(**env))
+    template['spec'] = spec
     return template
 
 
@@ -55,8 +66,8 @@ def inject_pod_ff(spec):
     return template
 
 
-def inject_pod_dind(spec):
-    with open(os.path.join(os.path.dirname(__file__), '../template/pod-dind.yaml'), 'rt') as f:
+def inject_pod_dind(spec, template_path):
+    with open(os.path.join(os.path.dirname(__file__), template_path), 'rt') as f:
         template = yaml.safe_load(f.read().format(**env))
     template['spec']['volumes'] += spec.get('volumes', [])
     template['spec']['containers'][0]['ports'] = [port for c in spec.get('containers', []) for port in
@@ -64,7 +75,7 @@ def inject_pod_dind(spec):
     template['spec']['containers'][0]['volumeMounts'] += [
         {'name': volume['name'], 'mountPath': f"/mount/{volume['name']}"}
         for volume in spec.get('volumes', [])]
-    template['spec']['containers'] += replace_container(spec, *read_docker_env(template))
+    template['spec']['containers'] += replace_container(spec, read_docker_env(template))
     template['metadata']['annotations'][CONTAINER_SPEC_ANNOTATION] = json.dumps(spec.get('containers', []))
     template['metadata']['annotations'][VOLUME_LIST_ANNOTATION] = json.dumps([f"/mount/{volume['name']}"
                                                                               for volume in spec.get('volumes', [])])
@@ -72,23 +83,21 @@ def inject_pod_dind(spec):
 
 
 def read_docker_env(template):
-    env_api_port = '8888'
     env_docker_host = '127.0.0.1:2375'
     for container in template['spec']['containers']:
-        if container['name'] == 'api-server':
-            env_api_port = container['ports'][0]['containerPort']
+        if container['name'] == 'runtime':
             for env_var in container['env']:
                 if env_var['name'] == 'DOCKER_HOST':
                     env_docker_host = env_var['value']
-    return env_docker_host, env_api_port
+    return env_docker_host
 
 
-def replace_container(spec, env_docker_host, env_api_port):
+def replace_container(spec, env_docker_host):
     return [{'name': container['name'],
-             'image': EXEC_MONITOR,
+             'image': AMBASSADOR_IMAGE,
              'env': [{'name': 'DOCKER_HOST', 'value': f'{env_docker_host}'},
                      {'name': 'CONTAINER_NAME', 'value': container['name']},
-                     {'name': 'API_SERVER', 'value': f'127.0.0.1:{env_api_port}'}],
+                     {'name': 'API_SERVER', 'value': f'127.0.0.1:8888'}],
              'imagePullPolicy': IMAGE_PULL_POLICY,
              'startupProbe': {'httpGet': {'port': 8888, 'path': f"/probe/{container['name']}"}, 'periodSeconds': 1}}
             for container in spec.get('containers', [])]
